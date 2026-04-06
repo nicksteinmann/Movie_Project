a3b4ae28122648416688b7cc612779371e38fbda
Nick Steinmann <nick.steinmann@gmx.de>
2026-04-06 14:26:15 +0200

    Extend movie storage with poster field

--- movie_storage_sql.py
+++ movie_storage_sql.py
@@ -24,7 +24,9 @@ with engine.connect() as connection:
                                 rating
                                 REAL
                                 NOT
-                                NULL
+                                NULL,
+                                poster
+                                TEXT
                             )
                             """))
     connection.commit()
@@ -34,23 +36,23 @@ def list_movies():
     """Retrieve all movies from the database."""
     with engine.connect() as connection:
         result = connection.execute(
-            text("SELECT title, year, rating FROM movies")
+            text("SELECT title, year, rating, poster FROM movies")
         )
         movies = result.fetchall()
 
-    return {row[0]: {"year": row[1], "rating": row[2]} for row in movies}
+    return {row[0]: {"year": row[1], "rating": row[2], "poster": row[3]} for row in movies}
 
 
-def add_movie(title, year, rating):
+def add_movie(title, year, rating, poster):
     """Add a new movie to the database."""
     with engine.connect() as connection:
         try:
             connection.execute(
                 text("""
-                     INSERT INTO movies (title, year, rating)
-                     VALUES (:title, :year, :rating)
+                     INSERT INTO movies (title, year, rating, poster)
+                     VALUES (:title, :year, :rating, :poster)
                      """),
-                {"title": title, "year": year, "rating": rating}
+                {"title": title, "year": year, "rating": rating, "poster": poster}
             )
             connection.commit()
             print(f"Movie '{title}' added successfully.")
